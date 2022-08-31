d0577429a1931e53e3f42192c4be44f5f34d6fb3
Nahuel Pedrozo <nahuel502010@gmail.com>
2022-08-31 19:51:34 -0300

    Noveno commit: Se agrega funcionamiento se hacen aparecer las imagenes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //Objetos -----------------------------------------------------------------------------------
-
 class Jugador{
     constructor(nombre, puntaje){
         this.nombre = nombre;
@@ -7,7 +6,6 @@ class Jugador{
     }
 
 }
-
 class Ficha{
     constructor(nombre, id, imagen){
         this.nombre = nombre;
@@ -15,14 +13,10 @@ class Ficha{
         this.imagen = imagen;
     }
 }
-
-
-
-//Fuciones -----------------------------------------------------------------------------
-const CrearFicha = (nombreObjeto) =>  nombreObjeto = new Ficha(nombreObjeto,Math.floor(Math.random()*100,"nada"))
-
+//----------------------------------------------------------------------------------------
+//Fuciones -------------------------------------------------------------------------------
+const CrearFicha = (nombreObjeto) =>  nombreObjeto = new Ficha(nombreObjeto,Math.floor(Math.random()*100,"nada"));
 const CrearJugador = (nombre) =>  new Jugador(nombre, 0);
-
 const Encontrar = (ficha1, ficha2) => { 
     if(ficha1.id === ficha2.id){
         return true;
@@ -31,70 +25,69 @@ const Encontrar = (ficha1, ficha2) => {
     }
 }
 const sumarPts= jugador=> jugador.puntaje++;
-//Variables-----------------------------------------------------------------------------
-const Jugadores = [];
-let opcionValida = true;
-const Fichas = [];
-
-//----------------------------------------------------------------------------------------
-
+const Iniciar = (e) =>{
+    e.preventDefault();
+    let datos = e.target;
+    Jugadores.push(CrearJugador(datos.children[1].value));
+    if(datos.children[3].checked){
+        CrearTablero(Number(datos.children[3].value))
+        console.log(datos.children[3].value);
+    }else if (datos.children[5].checked){
+        CrearTablero(Number(datos.children[5].value))
+    }else{
+        CrearTablero(Number(datos.children[7].value))
+    }
+}
 const CrearTablero = (cantidad)=>{
+    let numeroPieza=1;
+    Tablero.splice(0, Tablero.length);
+    Fichas.splice(0, Fichas.length)   
+    console.log(Fichas.length, Tablero.length);
     switch(cantidad){
         case 1:
             for(let i=0 ; i<2 ; i++){
                 Fichas.push(CrearFicha(i));
+                Tablero.push(Fichas[i]);
+                Tablero.push(Fichas[i]);
             } 
             break;
         case 2: 
             for(let i=0 ; i<4 ; i++){
                 Fichas.push(CrearFicha(i));
+                Tablero.push(Fichas[i]);
+                Tablero.push(Fichas[i]);
             } 
             break;
         case 3:
             for(let i=0 ; i<8 ; i++){
                 Fichas.push(CrearFicha(i));
+                Tablero.push(Fichas[i]);
+                Tablero.push(Fichas[i]);
             }
             break; 
-    } 
-}
-
-
-const Tablero = Fichas.concat(Fichas);
-const areaJuego= document.getElementsByClassName('juego')[1];
-console.log(areaJuego);
-let numeroPieza=1;
-for(pieza of Tablero ){
-    let img = document.createElement("img");
-    img.setAttribute("class", `ficha f${numeroPieza}` );
-    img.setAttribute("src","../img/volteado.jpg");
-    img.setAttribute("id", `f${numeroPieza}`);
-    console.log(img);
-    areaJuego.appendChild(img);
-    numeroPieza++;
-}
-console.log(areaJuego);
-/*const jugar = (jugador) =>{
-    let opcion1= Number(prompt("Ingrese el número de la ficha"));
-    let opcion2= Number(prompt("Ingrese el número de la ficha"));
-    while(Encontrar(opcion1, opcion2)){
-        indice1=Tablero.indexOf(ficha1.id);
-        Tablero.splice(indice1,1);
-        indice2=Tablero.indexOf(ficha2.id);
-        Tablero.splice(indice2,1);
-        opcion1=Number(prompt("Ingrese el número de la ficha"));;
-        opcion2=Number(prompt("Ingrese el número de la ficha"));;
     }
+    for(pieza of Tablero ){
+        let img = document.createElement("img");
+        img.setAttribute("class", `ficha f${numeroPieza}` );
+        img.setAttribute("src","../img/volteado.jpg");
+        img.setAttribute("id", `f${numeroPieza}`);
+        areaJuego.appendChild(img);
+        numeroPieza++;
+    }
+    
 }
-*/
-for (const jugador of Jugadores) {
-    console.log(jugador.nombre);
-    console.log(jugador.jugador);
-}
+//--------------------------------------------------------------------------------------
+//Variables-----------------------------------------------------------------------------
+const Jugadores = [];
+let opcionValida = true;
+const Fichas = [];
+const Tablero=[];
+const areaJuego= document.getElementsByClassName('juego')[1];
 let com = document.getElementById("com");
 let formJugador = document.getElementsByClassName('jugador')[0]; 
+let formularioJugador = document.getElementById("player1");
+//--------------------------------------------------------------------------------------
+//Eventos-------------------------------------------------------------------------------
 com.onclick = () => formJugador.style.cssText=`display: inline;`
-let jugar = document.getElementById("jugar");
-let player1 = document.getElementById("player1");
-let tamanio = document.querySelector("input[name=tamanio]:checked");
-jugar.onclick = () => Jugadores.push(CrearJugador(player1.value))
-//jugar.onclick = () =>CrearTablero(tamanio.values)
+formularioJugador.addEventListener("submit",Iniciar);
+//-------------------------------------------------------------------------------------
