10adf7663300c940f3db0c91406ed5aa1f4f622f
Nahuel Pedrozo <nahuel502010@gmail.com>
2022-09-08 22:46:40 -0300

    Decimo commit:Limpia tablero y agrega promesa de seleccionar ficha

--- js/main.js
+++ js/main.js
@@ -15,15 +15,9 @@ class Ficha{
 }
 //----------------------------------------------------------------------------------------
 //Fuciones -------------------------------------------------------------------------------
-const CrearFicha = (nombreObjeto) =>  nombreObjeto = new Ficha(nombreObjeto,Math.floor(Math.random()*100,"nada"));
-const CrearJugador = (nombre) =>  new Jugador(nombre, 0);
-const Encontrar = (ficha1, ficha2) => { 
-    if(ficha1.id === ficha2.id){
-        return true;
-    }else{
-        return false
-    }
-}
+const CrearFicha = nombreObjeto =>  nombreObjeto = new Ficha(nombreObjeto,Math.floor(Math.random()*100,"nada"));
+const CrearJugador = nombre =>  new Jugador(nombre, 0);
+const Encontrar = (ficha1, ficha2) =>  ficha1.id == ficha2.id ?  true :  false;
 const sumarPts= jugador=> jugador.puntaje++;
 const Iniciar = (e) =>{
     e.preventDefault();
@@ -66,28 +60,45 @@ const CrearTablero = (cantidad)=>{
             }
             break; 
     }
+    console.log(areaJuego.children);
+   while(areaJuego.firstChild){
+        areaJuego.removeChild(areaJuego.firstChild);
+   }
     for(pieza of Tablero ){
         let img = document.createElement("img");
         img.setAttribute("class", `ficha f${numeroPieza}` );
-        img.setAttribute("src","../img/volteado.jpg");
+        img.setAttribute("src","./img/volteado.jpg");
         img.setAttribute("id", `f${numeroPieza}`);
         areaJuego.appendChild(img);
         numeroPieza++;
     }
     
 }
+const turno = () =>{
+        return new Promise((resolve,reject)=>{
+                setTimeout(()=>{
+                    Encontrar(ficha)?resolve('Encontro ficha') :reject('No encontro ficha');
+                },2000)
+        })
+}
+
 //--------------------------------------------------------------------------------------
 //Variables-----------------------------------------------------------------------------
 const Jugadores = [];
-let opcionValida = true;
+//let opcionValida = true;
 const Fichas = [];
 const Tablero=[];
 const areaJuego= document.getElementsByClassName('juego')[1];
 let com = document.getElementById("com");
 let formJugador = document.getElementsByClassName('jugador')[0]; 
 let formularioJugador = document.getElementById("player1");
+let pvp = document.getElementById("pvp");
+//const fichasJuego= document.getElementsByClassName("fichas");
+//fichasJuego.onclick = () => 
 //--------------------------------------------------------------------------------------
 //Eventos-------------------------------------------------------------------------------
-com.onclick = () => formJugador.style.cssText=`display: inline;`
+com.onclick = () => formJugador.style.cssText=`display: inline;`;
+pvp.onclick = () => formJugador.style.cssText=`display: inline;`;
+
 formularioJugador.addEventListener("submit",Iniciar);
 //-------------------------------------------------------------------------------------
