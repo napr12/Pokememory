d761d3c82d665588adf2adbbdb44d30247e43c02
Nahuel Pedrozo <nahuel502010@gmail.com>
2022-10-04 21:14:12 -0300

    Decimotercer commit:Agrega API

--- js/main.js
+++ js/main.js
@@ -15,35 +15,60 @@ class Ficha{
 }
 //----------------------------------------------------------------------------------------
 //Fuciones -------------------------------------------------------------------------------
-const CrearFicha = nombreObjeto =>  nombreObjeto = new Ficha(nombreObjeto,Math.floor(Math.random()*100,"nada"));
-const CrearJugador = nombre =>  new Jugador(nombre, 0);
+const CrearFicha = async ()=>{
+    //nombreObjeto = new Ficha(nombreObjeto,Math.floor(Math.random()*100,"nada"));
+    const obtenerFicha = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${Math.floor(Math.random()*100)}/`);
+    const dataFicha = await obtenerFicha.json();
+    return new Ficha(dataFicha.name, dataFicha.id, dataFicha.sprites.front_default); 
+    
+}  
+
+const CrearJugador = nombre => new Jugador(nombre, 0) ;
 const Encontrar = (ficha1, ficha2) =>  ficha1.id == ficha2.id ?  true :  false;
 const sumarPts= jugador=> jugador.puntaje++;
-const Iniciar = (e) =>{
+const Iniciar = async (e) =>{
     e.preventDefault();
     let datos = e.target;
     Jugadores.push(CrearJugador(datos.children[1].value));
     if(datos.children[3].checked){
-        CrearTablero(Number(datos.children[3].value));
+        await CrearTablero(Number(datos.children[3].value));
         saludar(Jugadores[0].nombre);
     }else if (datos.children[5].checked){
-        CrearTablero(Number(datos.children[5].value));
+        await CrearTablero(Number(datos.children[5].value));
         saludar(Jugadores[0].nombre);
     }else if (datos.children[7].checked){
-        CrearTablero(Number(datos.children[7].value));
+        await CrearTablero(Number(datos.children[7].value));
         saludar(Jugadores[0].nombre);
     }else{
-        alertaError("DEBE SELECCIONAR UNA OPCIÓN VALIDA","REINTENTAR")
+        alertaError("DEBE SELECCIONAR UNA OPCIÓN VALIDA","REINTENTAR");
     }
-    
+    let tarjetas = document.getElementsByClassName("ficha");
+    console.log("Llega");
+    for (const element of tarjetas) {
+        element.onclick = (e) => { 
+            e.preventDefault();
+            let datos = e.target;
+            let posicion = Number(datos.id[1])-1;
+            datos.src= Tablero[posicion].imagen;
+            console.log(Tablero[posicion].imagen);   
+         };
+    }
 }
+/*
+const Voltear= (e) => { 
+    e.preventDefault();
+    let datos = e.target;
+    let posicion = Number(datos.id[1]);
+    datos.src= Tablero[posicion].imagen;
+    console.log("Entra");   
+ }*/
 const alertaError = (textoMensaje, buttonTexto) => {
     swal({
         text: textoMensaje,
         icon:"./img/Error.jpg",
         button:buttonTexto});
 }
-const CrearTablero = (cantidad)=>{
+const CrearTablero = async (cantidad) => {
     let numeroPieza=1;
     Tablero.splice(0, Tablero.length);
     Fichas.splice(0, Fichas.length)   
@@ -51,21 +76,21 @@ const CrearTablero = (cantidad)=>{
     switch(cantidad){
         case 1:
             for(let i=0 ; i<2 ; i++){
-                Fichas.push(CrearFicha(i));
+                Fichas.push( await CrearFicha());
                 Tablero.push(Fichas[i]);
                 Tablero.push(Fichas[i]);
             } 
             break;
         case 2: 
             for(let i=0 ; i<4 ; i++){
-                Fichas.push(CrearFicha(i));
+                Fichas.push(await CrearFicha());
                 Tablero.push(Fichas[i]);
                 Tablero.push(Fichas[i]);
             } 
             break;
         case 3:
             for(let i=0 ; i<8 ; i++){
-                Fichas.push(CrearFicha(i));
+                Fichas.push(await CrearFicha());
                 Tablero.push(Fichas[i]);
                 Tablero.push(Fichas[i]);
             }
@@ -91,7 +116,7 @@ const IniciarJuego = () =>{
         
     }
 }
-const turno = () =>{
+const turno =  () =>{
         return new Promise((resolve,reject)=>{
                 setTimeout(()=>{
                     Encontrar(ficha)?resolve('Encontro ficha') :reject('No encontro ficha');
@@ -117,7 +142,6 @@ const saludar = (nombreJugador) =>{
 //--------------------------------------------------------------------------------------
 //Variables-----------------------------------------------------------------------------
 const Jugadores = [];
-//let opcionValida = true;
 const Fichas = [];
 const Tablero=[];
 const areaJuego= document.getElementsByClassName('juego')[1];
@@ -125,12 +149,14 @@ let com = document.getElementById("com");
 let formJugador = document.getElementsByClassName('jugador')[0]; 
 let formularioJugador = document.getElementById("player1");
 let pvp = document.getElementById("pvp");
+
 //const fichasJuego= document.getElementsByClassName("fichas");
 //fichasJuego.onclick = () => 
 //--------------------------------------------------------------------------------------
 //Eventos-------------------------------------------------------------------------------
 com.onclick = () => formJugador.style.cssText=`display: inline;`;
 pvp.onclick = () => formJugador.style.cssText=`display: inline;`;
+
 
 formularioJugador.addEventListener("submit",Iniciar);
 //-------------------------------------------------------------------------------------
